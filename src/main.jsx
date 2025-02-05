72f94abb7a68d11ed582bde3f7f75eec0eadb0ab
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-02-04 21:25:17 -0300

    :construction: arquivo global

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
 import { SignUp } from './pages/SignUp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignUp />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <SignUp />
+    </ThemeProvider>
   </StrictMode>,
 )
