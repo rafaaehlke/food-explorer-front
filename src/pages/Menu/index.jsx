6a8a4982cbbaecee7341b791bf7572a40d547420
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-02-10 23:05:46 -0300

    :construction: modal menu

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,12 +1,25 @@
+import { Container, Section } from './styles'
 import { HeaderMenu } from '../../components/HeaderMenu'
-import { Container } from './styles'
+import { Footer } from '../../components/Footer'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+
+
 
 export function Menu() {
   return (
     <Container>
 
-     <HeaderMenu />
+      <HeaderMenu />
 
+
+      <Section>
+        <Input placeholder="Buscar pratos ou ingredientes" title="Buscar pratos ou ingredientes" />
+        <Button id="exit" title="Sair"/>
+      </Section>
+
+      <Footer />
     </Container>
   )
 }
--- src/pages/Menu/styles.js
+++ src/pages/Menu/styles.js
@@ -1,7 +1,37 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreackPoints"
+
 
 export const Container = styled.div`
-  border: 1px daseh red;
+margin: 0 auto;
+min-height: 100vh;
+`
 
+
+export const Section = styled.section`
+  min-height: 42.25rem;
+
+  margin-top: 2.2rem;
+  margin-left: 1.75rem;
+  margin-right: 1.75rem;
   
+ 
+> Button {
+  
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding: .62rem .62rem;
+
+  font-size: 1.5rem;
+  font-weight: 300;
+  color: ${({ theme }) => theme.COLORS.LIGHT_300};
+
+  background-color: transparent;
+}
+
+@media (min-width: ${DEVICE_BREAKPOINTS.MD}) {
+  margin-bottom: 2.8rem;
+}
+
 `
