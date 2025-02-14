937fa517bbb06caf455f307c37f2605ee042bce5
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-02-13 21:15:23 -0300

    :construction: corrigido propriedades do button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,19 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  gap: .5rem;
   width: 100%;
-  background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
-  color: ${({ theme }) => theme.COLORS.LIGHT_100};
-
-
   height: 3rem;
+  
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  
   border: none;
   padding: 0.75rem 2rem;
-  margin-top: 2rem;
   border-radius: 0.3125rem;
-  font-weight: 500;
-  font-family: 'Poppins', sans-serif;
   
+  color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
   &:disabled {
     opacity: 0.5;
   }
