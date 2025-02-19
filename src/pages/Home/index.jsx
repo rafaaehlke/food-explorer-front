35d489a5e6bd3dc0be591f08c41dc30647b44dd1
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-02-18 22:25:19 -0300

    :construction: Criado cardapio com animacao, falta correcao

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,10 +4,22 @@ import { Footer } from '../../components/Footer'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import Principal from '../../assets/pngegg.png'
+import Salada from '../../assets/salada.png'
+import Pao from '../../assets/paozinho.png'
+import Suco from '../../assets/sucocanela.png'
 
+
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
 
 
 export function Home() {
+  const [sliderRef, instanceRef] = useKeenSlider({
+    slideChanged: () => {
+      console.log('Slide alterado');
+    },
+  })
+
   return (
     <Container>
       <HeaderWrapper>
@@ -16,21 +28,78 @@ export function Home() {
 
 
       <section className="flyer">
-        <img src={Principal} alt="" />
-        <p>Sabores inigualáveis</p>
-        <span>Sinta o cuidado do preparo com ingredientes selecionados.</span>
+        <img src={Principal} alt="Logo flyer" />
+        <div className="wrapper-flyer">
+          <p>Sabores inigualáveis</p>
+          <span>Sinta o cuidado do preparo com ingredientes selecionados.</span>
+        </div>
       </section>
 
       <section className="meals">
-        <p>Refeiçoes</p>
+        <p>Refeições</p>
+
+          {/* Refeicoes 1 */}
+        <div ref={sliderRef} className="keen-slider">
+          <div className="keen-slider__slide">
+            <img src={Salada} alt="" />
+            <p>Salada Ravanello</p>
+            <span>R$ 49,97</span>
+            <Button title="incluir"></Button>
+          </div>
+
+          {/* Refeicoes 1 */}
+          <div className="keen-slider__slide">
+            <img src={Salada} alt="" />
+            <p>Salada</p>
+            <span>R$ 49,97</span>
+            <Button title="incluir"></Button>
+          </div>
+        </div>
       </section>
 
       <section className="mainDishes">
         <p>Pratos Principais</p>
+
+        <div ref={sliderRef} className="keen-slider">
+         
+         {/* Prato 1 */}
+          <div className="keen-slider__slide">
+            <img src={Pao} alt="" />
+            <p>Misto Quente</p>
+            <span>R$ 49,97</span>
+            <Button title="incluir"></Button>
+          </div>
+
+        {/* Prato 2 */}
+          <div className="keen-slider__slide">
+            <img src={Principal} alt="" />
+            <p>Salada</p>
+            <span>R$ 49,97</span>
+            <Button title="incluir"></Button>
+          </div>
+        </div>
       </section>
 
-      <section className="mainDishesTwo">
-        <p>Pratos Principais</p>
+      <section className="drinks">
+        <p>Bebidas</p>
+        <div ref={sliderRef} className="keen-slider">
+          
+          {/* Suco 1 */}
+          <div className="keen-slider__slide">
+            <img src={Suco} alt="" />
+            <p className='drink'>Suco Maracuja</p>
+            <span>R$ 49,97</span>
+            <Button title="incluir"></Button>
+          </div>
+
+          {/* Suco 2 */}
+          <div className="keen-slider__slide">
+            <img src={Principal} alt="" />
+            <p>Salada</p>
+            <span>R$ 49,97</span>
+            <Button title="incluir"></Button>
+          </div>
+        </div>
       </section>
 
       <FooterWrapper>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -10,10 +10,12 @@ width: 100vw;
 
 
 export const Container = styled.div`
+height: 100vh;
 display: grid;
 grid-template-columns: repeat(1, 1fr);
 grid-template-rows: repeat(6, 1fr);
-height: 100vh;
+row-gap: 2.4rem;
+
 grid-template-areas: 
 "header" "header"
 "flyer" "flyer"
@@ -24,20 +26,19 @@ grid-template-areas:
 
 
 
-.flyer {
-  border: 1px dashed green;
-  
+.flyer { 
   grid-area: flyer;
   grid-row: 2/3;
   
   width: 37.6rem;
   height: 12.0rem;
-  margin: 4.4rem 1.6rem auto 3.6rem;
+  margin-left: 3.0rem;
+  margin-top: 2.4rem;
+  margin-right: 1.6rem;
   
   background: var(--Gradients-200, linear-gradient(180deg, #091E26 0%, #00131C 100%));
   
-  position: absolut;
-  z-index: 1;
+ 
   img {
     width: 19.1rem;
     height: 14.9rem;
@@ -48,47 +49,123 @@ grid-template-areas:
     bottom: 26%;
   }
   
-  > p, span {
-    position: relative;
-    display: flex;
-    justify-content: space-evenly;
-  }
+ 
+}
+
+.wrapper-flyer {
+  width: 21.5rem;
+  display: flex;
+  align-items: flex-start;
+  align-content: flex-start;
+  flex-direction: column;
+  position: relative;
+  z-index: 2;
+  left: 44%;
+  top: -100%;
 
   p {
     width: 21.5rem;
+    height: 2.5rem;
+    
+    font-family: "Poppins", serif;
     font-size: 1.8rem;
     line-height: 2.5rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
   }
+  
   span {
     width: 20.2rem;
+    height: 3.4rem;
+
+    font-family: "Poppins", serif;
     font-size: 1.2rem;
     line-height: 1.6rem;
+
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    
   }
 }
 
+// Refeiçoes
 .meals {
-  border: 1px dashed red;
-  
   grid-area: meals;
   grid-row: 3/4;
+  width: 21.0rem;
+  height: 29.2rem;
+  
+  img {
+    width: 8.8rem;
+    height: 8.8rem;
+    margin: 2.4rem 6.1rem;
+  }
+
+span {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1rem;
+
+  color: ${({theme}) => theme.COLORS.CAKE_200};
+  margin: 2.4rem 6.1rem;
+  }
+
+}
+
+.keen-slider {
+  margin: 2.4rem 2.4rem;
+  background-color: ${({ theme }) => theme.COLORS.DARK_200};
+
+  border-radius: .8rem;
+  border: 1px solid ${({theme}) => theme.COLORS.DARK_300};
 }
 
 .mainDishes {
-  border: 1px dashed blue;
-
   grid-area: mainDishes;
   grid-row: 4/5;
 
-  width: 376px;
-  height: 7.5rem;
+  width: 21.0rem;
+  height: 29.2rem;
+
+  img {
+    width: 8.8rem;
+    height: 8.8rem;
+  
+    margin: 2.4rem 6.1rem;
+  }
 }
 
-.mainDishesTwo {
+.drinks {
   grid-area: mainDishes;
   grid-row: 5/5;
+  width: 21.0rem;
+  height: 29.2rem;
   
+  img {
+    width: 8.8rem;
+    height: 8.8rem;
+  
+    margin: 2.4rem 6.1rem;
+  }
+
+  .drink {
+  text-align: center
+  }
 }
 
+// Estilização padrão
+.meals,
+.mainDishes,
+.drinks { 
+  p {
+    font-family: Poppins;
+    font-size: 1.8rem;
+    font-weight: 500;
+    line-height: 2.5rem;
+    color: ${({theme}) => theme.COLORS.LIGHT_300};
+    margin-left: 2.4rem; 
+  }
+}
 `
 
 export const FooterWrapper = styled.div`
