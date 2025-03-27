0c39094d7c6f001fd8756e2afa8ea6b26580bf13
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-26 21:44:09 -0300

    :construction: useState para user,email, password

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,17 +1,23 @@
-import Logo from "../../assets/logo.svg"
-import { Container, Form } from "./styles"
-import { Input } from "../../components/Input"
+import Logo from "../../assets/logo.svg";
+import { Container, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { useState } from "react";
 
-import { Button } from "../../components/Button"
+import { api } from "../../services/api"
+
+import { Button } from "../../components/Button";
 import { useNavigate } from 'react-router-dom';
 
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
 
-  const haveAccount = () => {
-    navigate('/')
+  const handleSignUp = () => {
+    console.log(name, email, password)
   }
 
   return (
@@ -26,15 +32,29 @@ export function SignUp() {
         <div id="titleForm">
           <h1>Crie sua conta</h1>
         </div>
-        <span>Seu nome</span>
-        <Input placeholder="Exemplo: Rocktseat"></Input>
-        <span>Email</span>
-        <Input placeholder="Exemplo: rocketseat@email.com"></Input>
-        <span>Senha</span>
-        <Input placeholder="Mínimo 6 caracteres"></Input>
 
-        <Button id="" title="Criar Conta" />
-        <Button id="haveAccount" title="Já tenho uma conta" onClick={haveAccount}/>
+        <span>Seu nome</span>
+        <Input
+          placeholder="Exemplo: Rocktseat"
+          type="text"
+          onChange={e => setName(e.target.value)}
+        />
+
+        <span>Email</span>
+        <Input
+          placeholder="Exemplo: rocketseat@email.com"
+          type="email"
+        onChange={e => setEmail(e.target.value)}
+        />
+
+        <span>Senha</span>
+        <Input placeholder="Mínimo 6 caracteres"
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <Button id="" title="Criar Conta" onClick={handleSignUp}/>
+        <Button id="haveAccount" title="Já tenho uma conta" />
 
       </Form>
 
