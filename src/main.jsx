bfc4529b7262a6a8399d72ac90c5e2bacf957c5d
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-03-27 21:23:15 -0300

    :sparkles: AuthProvider englobando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import theme from './styles/theme'
-import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components';
+import { createRoot } from 'react-dom/client';
+import GlobalStyles from './styles/global';
+import { AuthProvider } from './hooks/auth';
+import { StrictMode } from 'react';
+import theme from './styles/theme';
 
 
-import { Home } from './pages/Home'
+import { Routes } from './routes'
+// import { PagePrato } from './pages/PagePrato'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-     <Home />
+      <AuthProvider>
+        <GlobalStyles />
+        < Routes />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 )
