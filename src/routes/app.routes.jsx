c9307a2f446076c0573ada02e5c98a7a0120ba55
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-04-14 00:47:31 -0300

    :construction: rotas de pagina

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,13 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 
-import { Menu } from '../pages/Menu'
 import { Home } from '../pages/Home'
+import { Menu } from '../pages/Menu'
+import { PagePrato } from '../pages/PagePrato'
+import { NewPrato } from '../pages/NewPrato'
+import { EditPrato } from '../pages/EditPrato'
+import { Profile } from '../pages/Profile'
+
+
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/menu' element={<Menu />} />
       <Route path='/' element={<Home />} />
+      <Route path='/menu' element={<Menu />} />
+      <Route path='/pagePrato' element={<PagePrato />} />
+      <Route path='/newPrato' element={<NewPrato />} />
+      <Route path='/editPrato' element={<EditPrato />} />
+      <Route path='/profile' element={<Profile />} />
+
+
     </Routes>
   )
 }
