bf65d1d25132669d697927e7861cd5124e70813a
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-04-14 00:45:17 -0300

    :construction: UpdateProfile hook

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -39,6 +39,23 @@ function AuthProvider({ children }) {
     setData({});
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put("/users", user)
+      localStorage.setItem("@foodexplorer:user", JSON.stringify(user));
+      setData({ user, token: data.token });
+      alert("Perfil atualizado.")
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+
+      } else {
+        alert("Não foi possivel atualizar o perfil.");
+      }
+    }
+  }
+
   // verificar se há token e usuário no localStorage ao montar o componente
   useEffect(() => {
     const token = localStorage.getItem("@foodexplorer:token");
@@ -56,7 +73,12 @@ function AuthProvider({ children }) {
 
 
   return (
-    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+    <AuthContext.Provider value={{
+      signIn,
+      signOut,
+      updateProfile,
+      user: data.user
+    }}>
       {children}
     </AuthContext.Provider>
   );
@@ -65,5 +87,7 @@ function AuthProvider({ children }) {
 function useAuth() {
   const context = useContext(AuthContext);
 
-  return context;}  
+  return context;
+}
+
 export { AuthProvider, useAuth };
