0ebbc66ea503ef17055fc612695174cd99f20603
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-04-28 21:37:14 -0300

    :construction: Image, Navigate, Ingredients adcionados

--- src/pages/NewPrato/index.jsx
+++ src/pages/NewPrato/index.jsx
@@ -4,26 +4,53 @@ import { Button } from '../../components/Button';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Input } from "../../components/Input";
+import { useNavigate } from "react-router-dom";
 import { PiCaretLeft } from "react-icons/pi";
-import { useAuth } from "../../hooks/auth";
+import { useAuth } from '../../hooks/auth';
 import { LuUpload } from "react-icons/lu";
+import { api } from "../../services/api";
 import { useState } from "react";
+
 
 export function NewPrato() {
   const { user } = useAuth();
-  //console.log("Usuário logado:", user);
+  console.log("usuário logado", user)
 
-  const [ingredientes, setIngredientes] = useState([]);
+  const [imageName, setImageName] = useState("Selecione uma imagem");
+  const [image, setImage] = useState("");
+
+  const [ingredientes, setIngredientes] = useState([""]);
   const [novoIngrediente, setNovoIngrediente] = useState("");
 
+  const navigate = useNavigate();
+
+  function voltarMenu() {
+    navigate("/");
+  };
+
+  //OK
+  function handleAddImage() {
+    const file = event.target.files[0];
+    
+    if(file){
+      setImage(file);
+      setImageName(file.name);
+    }
+  };
+
+  //OK
   function handleAddIngrediente() {
     setIngredientes(estadoAnterior => [...estadoAnterior, novoIngrediente]);
-    setNovoIngrediente("")
+    setNovoIngrediente()
   }
 
+  //OK
   function handleRemoveIngrediente(deleted) {
     setIngredientes(estadoAnterior => estadoAnterior.filter(ingredientes => ingredientes !== deleted));
   }
+
+ 
+
   return (
     <Container>
       <HeaderWrapper>
@@ -35,7 +62,9 @@ export function NewPrato() {
         <Button
           className='voltar'
           title="Voltar"
-          icon={PiCaretLeft}>
+          icon={PiCaretLeft}
+          onClick={voltarMenu}
+        >
         </Button>
 
         <h1>Adicionar Prato</h1>
@@ -46,11 +75,11 @@ export function NewPrato() {
             <span>Imagem do prato</span>
             <label htmlFor="pictureDish">
               <LuUpload />
-              selecione uma imagem
+               {imageName}
               <input
                 type="file"
                 id="pictureDish"
-                title="Selecione uma imagem"
+                onChange={handleAddImage}
               />
             </label>
           </div>
@@ -58,7 +87,11 @@ export function NewPrato() {
           {/* Nome prato */}
           <div className="name-wrapper">
             <span id="name">Nome</span>
-            <input type="text" placeholder="Ex: Salada Crispy" id="dishName" />
+            <input
+              type="text"
+              placeholder="Ex: Salada Crispy"
+              id="dishName"
+            />
           </div>
 
           {/* Categoria */}
@@ -66,7 +99,10 @@ export function NewPrato() {
             <span id="category">Categoria</span>
             <div className="input-wrapper">
               <select id="selectCategory">
-                <option value="dish">Refeição</option>
+                <option value="refeicoes">Refeições</option>
+                <option value="sobremesa">Sobremesas</option>
+                <option value="janta">Jantas</option>
+                <option value="drinks">Drinks</option>
               </select>
             </div>
           </div>
@@ -97,13 +133,19 @@ export function NewPrato() {
           {/* Preço */}
           <div className="price-wrapper">
             <span className="price">Preço</span>
-            <Input id="price" placeholder="R$ 40,00" />
+            <Input
+              id="price"
+              placeholder="R$ 40,00"
+            />
           </div>
         </div>
 
         {/* Descriçao */}
         <span id="description">Descrição</span>
-        <textarea className="description" placeholder="A Salada César é uma opção refrescante para o verão." />
+        <textarea 
+        className="description" 
+        placeholder="A Salada César é uma opção refrescante para o verão." 
+        />
 
         <div className="buttons">
           <Button className="save" title="Salvar Alterações" />
