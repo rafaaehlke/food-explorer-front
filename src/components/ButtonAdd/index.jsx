5927ed48e0c0ea8c7af2be3cc13a9b21377bf112
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-05-06 21:23:34 -0300

    :construction: Adicionar e remover +1 ou -1, button

--- src/components/ButtonAdd/index.jsx
+++ src/components/ButtonAdd/index.jsx
@@ -1,17 +1,30 @@
 import { Container, Remove, Add } from "./style"
 import { FiMinus, FiPlus } from "react-icons/fi";
+import { useState } from "react";
 
 
 export function ButtonAdd({ ...props }) {
+  const [add, setAdd] = useState(1);
+
+  function addOne() {
+    setAdd(prev => prev + 1);
+  }
+
+  function removeOne() {
+    if (add > 1) {
+      setAdd(prev => prev - 1);
+    }
+  }
+
   return (
     <Container {...props}>
-      <Remove>
+      <Remove onClick={() => removeOne()}>
         <FiMinus />
       </Remove>
 
-      <span>01</span>
-     
-      <Add>
+      <span>{add}</span>
+
+      <Add onClick={() => addOne()}>
         <FiPlus />
       </Add>
     </Container>
