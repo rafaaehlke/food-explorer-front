cbccd7c73dee78a050faabbbfddba80c9b16c49f
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-05-15 22:23:33 -0300

    :construction: recuperando dados do prato

--- src/pages/EditPrato/index.jsx
+++ src/pages/EditPrato/index.jsx
@@ -1,24 +1,34 @@
 import { Container, HeaderWrapper, Form, FooterWrapper } from "./styles";
 import { NewIngredient } from "../../components/NewIngredient";
+import { useNavigate, useParams } from "react-router-dom";
 import { Button } from '../../components/Button';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Input } from "../../components/Input";
 import { PiCaretLeft } from "react-icons/pi";
+import { useEffect, useState } from "react";
 import { LuUpload } from "react-icons/lu";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/auth";
-
+import { api } from '../../services/api';
 
 export function EditPrato() {
 
-  const { user } = useAuth;
-  console.log("usuario:", user)
+  const { id } = useParams();
+
+
+  const [prato, setPrato] = useState("")
+  console.log("dados", prato)
+
   const navigate = useNavigate();
 
   function voltarMenu() {
     navigate("/");
   };
+
+  useEffect(() => {
+    api.get(`/dishes/${id}`)
+      .then(response => setPrato(response.data))
+      .catch(error => console.error("erro ao buscar o prato", error))
+  }, [id])
 
   return (
     <Container>
@@ -55,7 +65,12 @@ export function EditPrato() {
           {/* Nome prato */}
           <div className="name-wrapper">
             <span id="name">Nome</span>
-            <input type="text" placeholder="Ex: Salada Crispy" id="dishName" />
+            <input
+              id="dishName"
+              type="text"
+              placeholder={prato.name}
+              value={prato.name}
+            />
           </div>
 
           {/* Categoria */}
@@ -83,13 +98,21 @@ export function EditPrato() {
           {/* Preço */}
           <div className="price-wrapper">
             <span className="price">Preço</span>
-            <Input id="price" placeholder="R$ 40,00" />
+            <Input
+              id="price"
+              placeholder={prato.price}
+              value={prato.price}
+            />
           </div>
         </div>
 
         {/* Descriçao */}
         <span id="description">Descrição</span>
-        <textarea className="description" placeholder="A Salada César é uma opção refrescante para o verão." />
+        <textarea
+          className="description"
+          placeholder={prato.description}
+          value={prato.description}
+        />
 
         <div className="buttons">
           <Button className="delete" title="Excluir Prato" />
