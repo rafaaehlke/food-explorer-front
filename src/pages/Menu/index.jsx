47b559649b3b9918d7fcc20fdcf6d5797b09b933
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-05-30 20:05:40 -0300

    :construction: criacao e personalizacao styles, input search menu

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -4,13 +4,18 @@ import { Footer } from '../../components/Footer'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { IoIosSearch } from "react-icons/io";
-import { useAuth } from '../../hooks/auth'
-import { useNavigate } from 'react-router-dom'
-import { USER_ROLE } from '../../utils/roles'
+import { useNavigate } from 'react-router-dom';
+import { USER_ROLE } from '../../utils/roles';
+import { useState, useEffect } from 'react';
+import { useAuth } from '../../hooks/auth';
+import { api } from '../../services/api';
 
 
 export function Menu() {
   const { user } = useAuth()
+  const [search, setSearch] = useState("")
+  const [dishes, setDishes] = useState([])
+  const [showDishes, setShowDishes] = useState(false)
 
   const navigate = useNavigate();
 
@@ -26,6 +31,36 @@ export function Menu() {
     navigate("/Profile")
   }
 
+  function handleSelect(prato) {
+    navigate(`/prato/${prato.id}`);
+    setSearch("");
+    setShowDishes(false);
+  }
+
+  useEffect(() => {
+    const fetchDishes = async () => {
+      if (search.trim() === "") {
+        setDishes([])
+        return;
+      }
+
+      try {
+        const response = await api.get(`/dishes?search=${search}`)
+        setDishes(response.data)
+        setShowDishes(true)
+      } catch (error) {
+        console.error("Erro ao buscar um produto.", error)
+        alert("Nenhum prato encontrado!")
+      }
+    };
+
+    const debounce = setTimeout(() => {
+      fetchDishes()
+    }, 500);
+
+    return () => clearTimeout(debounce)
+  }, [search])
+
   return (
     <Container>
 
@@ -35,8 +70,33 @@ export function Menu() {
       {
         [USER_ROLE.ADMIN, USER_ROLE.CLIENTE].includes(user.role) &&
         <Section>
-          <Input icon={IoIosSearch} placeholder="Buscar pratos ou ingredientes" title="Buscar pratos ou ingredientes" />
+            <Input
+              icon={IoIosSearch}
+              placeholder="Buscar pratos ou ingredientes"
+              title="Buscar pratos ou ingredientes"
+              onChange={e => setSearch(e.target.value)}
+              onFocus={() => setShowDishes(true)}
+              onBlur={() => setTimeout(() => setShowDishes(false), 200)}
+            />
 
+            {/* DropDown search menu */}
+          <div className="search">
+            {
+              showDishes && dishes.length && (
+                <ul>
+                  {
+                    dishes.map((pratos) =>
+                      <li
+                        key={pratos.id}
+                        onMouseDown={(e) => e.preventDefault()}
+                        onClick={() => handleSelect(pratos)}
+                      >
+                        {pratos.name}
+                      </li>
+                    )}
+                </ul>
+              )}
+          </div>
           {user.role === USER_ROLE.ADMIN && <Button id="editarPratos" title="Editar Pratos" onClick={handleEditPrato} />}
           {user.role === USER_ROLE.ADMIN && <Button id="cadastrarPratos" title="Novo Prato" onClick={newPrato} />}
           {(user.role === USER_ROLE.CLIENTE || USER_ROLE.ADMIN) && <Button id="profile" title="Perfil" onClick={handleProfile} />}
--- src/pages/Menu/styles.js
+++ src/pages/Menu/styles.js
@@ -14,6 +14,39 @@ export const Section = styled.section`
   margin-top: 2.2rem;
   margin-left: 1.75rem;
   margin-right: 1.75rem;
+
+  .search {
+  position: relative;
+  color: ${({ theme }) => theme.COLORS.LIGHT_400};
+
+  
+  ul {
+    position: absolute;
+    list-style: none;
+    top: 105%;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    
+    
+  background-color: ${({ theme }) => theme.COLORS.DARK_1000};
+  margin: 0;
+  max-height: 200px;
+  overflow-y: auto;
+  border-radius: .8rem;
+  padding: .8rem .8rem; 
+  scrollbar-width: thin;
+  scrollbar-color: ${({ theme }) => theme.COLORS.LIGHT_400} ${({ theme }) => theme.COLORS.DARK_1000};
+  }
+
+  li {
+  border: 1px dashed ${({ theme }) => theme.COLORS.DARK_800};
+  padding: 1rem 1rem;
+  cursor: pointer;
+  
+ }
+}
+ 
   
  
 > Button {
